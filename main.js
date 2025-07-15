dfb93fae6c0009081f92e9132522fab3c53f8475
Neelan <neelan2317@gmail.com>
2025-07-14 23:14:17 -0400

    Fix some stuff

--- main.js
+++ main.js
@@ -4,24 +4,26 @@ import { FlashCardList } from './entities/FlashCardList.js';
 import { FlashCard } from './entities/FlashCard.js';
 
 
+window.addEventListener("load", function () {
+    const BossMans_FlashCardList = new FlashCardList();
 
-const BossMans_FlashCardList = new FlashCardList();
+    const question = document.getElementById("question");
+    const answer = document.getElementById("answer");
 
-const question = document.getElementById("question");
-const answer = document.getElementById("answer");
+    const submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", submitFlashCard);
 
-const submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", submitFlashCard);
+    function submitFlashCard(){
+        console.log("RAN");
+        const input = new AddFlashCardInputData(question.value, answer.value);
+        const interactor = new AddFlashCardInteractor(BossMans_FlashCardList);
+        const newFlashCard = interactor.execute(input);
+        const para = document.createElement("p");
 
-function submitFlashCard(){
-  console.log("RAN");
-  const input = new AddFlashCardInputData(question.value, answer.value);
-  const interactor = new AddFlashCardInteractor(BossMans_FlashCardList);
-  const newFlashCard = interactor.execute(input);
-  const para = document.createElement("p");
+        para.innerText = "Question " + newFlashCard.getQuestion + "Answer " + newFlashCard.getAnswer + "Id " + newFlashCard.getId;
+        //console.log("Question " + newFlashCard.getQuestion + " Answer " + newFlashCard.getAnswer);
+        document.body.appendChild(para);
+    }
+    
+});
 
-  para.innerText = "Question " + newFlashCard.getQuestion + "Answer " + newFlashCard.getAnswer + "Id " + newFlashCard.getId;
-  //console.log("Question " + newFlashCard.getQuestion + " Answer " + newFlashCard.getAnswer);
-  document.body.appendChild(para);
-}
-   
