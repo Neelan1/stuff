b1fc37131861b6cc958ee8f8e2d549a8e353ab83
Daniel-Tann <102365167+Daniel-Tann@users.noreply.github.com>
2025-08-22 16:41:00 -0400

    Fixed bugs with the screens and worked on the View deck screen

--- entities/Deck.js
+++ entities/Deck.js
@@ -1,7 +1,27 @@
-class Deck{
-  list = []
+export class Deck{
+  flashCardList = [];
   
-  get getFlashCardLists(){
-    return this.list;
+  get flashCardLists(){
+    return this.flashCardList;
+  }
+/**
+ * Adds the FlashCardList to the deck when givent he object
+ * @param {*} flashCardList 
+ */
+  addFlashCardList(flashCardList){
+    console.log("Running addFlashCardList");
+    this.flashCardList.push(flashCardList);
+  }
+
+  /**
+   * This method removed the FlashCardList from the deck when given the name
+   * @param {*} name 
+   */
+  removeFlashCardList(name){
+    for(i = 0; i < this.flashCardList.length; i++){
+      if(this.flashCardList[i].getName == name){
+          this.flashCardList.splice(i, 1);
+      }
+    }
   }
 }
--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 import { AddFlashCardInputData } from './usecases/AddFlashCard/AddFlashCardInputData.js';
 import { AddFlashCardInteractor } from './usecases/AddFlashCard/AddFlashCardInteractor.js';
-import { FlashCardList } from './entities/FlashcardList.js';
+import { FlashCardList } from './entities/FlashCardList.js';
 import { FlashCard } from './entities/FlashCard.js';
 import { Presenter } from './frameworks/Presenter.js';
-import { ViewModel } from './frameworks/ViewFlashCardLists.js';
+import { ViewFlashCardLists } from './frameworks/ViewFlashCardLists.js';
 
-const view = new ViewModel();
-const presenter = new Presenter(view);
-const BossMans_FlashCardList = new FlashCardList();
-const interactor = new AddFlashCardInteractor(BossMans_FlashCardList, presenter);
-
-
+// const view = new ViewFlashCardLists();
+// const presenter = new Presenter(view);
+// const BossMans_FlashCardList = new FlashCardList();
+// const interactor = new AddFlashCardInteractor(BossMans_FlashCardList, presenter);
 
 
 
-const question = document.getElementById("question");
-const answer = document.getElementById("answer");
 
-const submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", submitFlashCard);
 
-function submitFlashCard(){
-  console.log("RAN");
-  const newFlashCard = document.createElement("p");
-  document.body.appendChild(newFlashCard);
-  const input = new AddFlashCardInputData(question.value, answer.value, newFlashCard);
+// const question = document.getElementById("question");
+// const answer = document.getElementById("answer");
+
+// const submitButton = document.getElementById("submit");
+// submitButton.addEventListener("click", submitFlashCard);
+
+// function submitFlashCard(){
+//   console.log("RAN");
+//   const newFlashCard = document.createElement("p");
+//   document.body.appendChild(newFlashCard);
+//   const input = new AddFlashCardInputData(question.value, answer.value, newFlashCard);
   
 
-  //Add <p> element here with ID
-  //Pass id into interactor.execute 
-  interactor.execute(input, presenter);
+//   //Add <p> element here with ID
+//   //Pass id into interactor.execute 
+//   interactor.execute(input, presenter);
 
   
-}
+// }
    
